0fa537c8c936e65ead49064eebf5efa96f14dbaa
MaiconMaciel <maicon.costa1331@gmail.com>
2025-11-08 13:17:41 -0300

    Atualização automática do tracker

--- scripts/data_load.py
+++ scripts/data_load.py
@@ -1,4 +1,5 @@
-
+import pandas as pd
+import datetime
 
 def git_commit_push(commit_message="Atualização automática do tracker"):
     import subprocess
@@ -8,10 +9,23 @@ def git_commit_push(commit_message="Atualização automática do tracker"):
 
         subprocess.run(["git", "commit", "-m", commit_message], check=True)
 
-        subprocess.run(["git", "push"], check=True)
+        subprocess.run(["git", "push"], check=True)     
 
-        print("Commit e push realizados com sucesso!")
     except subprocess.CalledProcessError as e:
-        print("Erro ao executar git:", e)
+        print("Erro git:", e)
+
+    else:
+        #log de push - com data e hora ? só pra ter sla
+        df = pd.read_csv("./logs/git.csv")
+
+        date = datetime.now(ZoneInfo="America/Sao_Paulo")
+        push = True 
+
+        df.append({
+            "data": date,
+            "push": push
+        })
+
+        print("Commit e push sucesso!") 
 
 git_commit_push()
